2f2ce73c4226ff725f5428008e229ebd5d766dfc
softkar <125498290+softkar@users.noreply.github.com>
2024-12-22 15:26:11 +0300

    Создаем человеков.py

--- Создаем человеков.py
+++ Создаем человеков.py
@@ -1,31 +1,75 @@
 import file_operations
 import os
 from faker import Faker
-from random import randint,sample
+from random import randint, sample
 
 runic_alphabet = {
-    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
-    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
-    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
-    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
-    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
-    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
-    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
-    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
-    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
-    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
-    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
-    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
-    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
-    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
-    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
-    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
-    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
-    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
-    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
-    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
-    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
-    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    'а': 'а͠',
+    'б': 'б̋',
+    'в': 'в͒͠',
+    'г': 'г͒͠',
+    'д': 'д̋',
+    'е': 'е͠',
+    'ё': 'ё͒͠',
+    'ж': 'ж͒',
+    'з': 'з̋̋͠',
+    'и': 'и',
+    'й': 'й͒͠',
+    'к': 'к̋̋',
+    'л': 'л̋͠',
+    'м': 'м͒͠',
+    'н': 'н͒',
+    'о': 'о̋',
+    'п': 'п̋͠',
+    'р': 'р̋͠',
+    'с': 'с͒',
+    'т': 'т͒',
+    'у': 'у͒͠',
+    'ф': 'ф̋̋͠',
+    'х': 'х͒͠',
+    'ц': 'ц̋',
+    'ч': 'ч̋͠',
+    'ш': 'ш͒͠',
+    'щ': 'щ̋',
+    'ъ': 'ъ̋͠',
+    'ы': 'ы̋͠',
+    'ь': 'ь̋',
+    'э': 'э͒͠͠',
+    'ю': 'ю̋͠',
+    'я': 'я̋',
+    'А': 'А͠',
+    'Б': 'Б̋',
+    'В': 'В͒͠',
+    'Г': 'Г͒͠',
+    'Д': 'Д̋',
+    'Е': 'Е',
+    'Ё': 'Ё͒͠',
+    'Ж': 'Ж͒',
+    'З': 'З̋̋͠',
+    'И': 'И',
+    'Й': 'Й͒͠',
+    'К': 'К̋̋',
+    'Л': 'Л̋͠',
+    'М': 'М͒͠',
+    'Н': 'Н͒',
+    'О': 'О̋',
+    'П': 'П̋͠',
+    'Р': 'Р̋͠',
+    'С': 'С͒',
+    'Т': 'Т͒',
+    'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠',
+    'Х': 'Х͒͠',
+    'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠',
+    'Ш': 'Ш͒͠',
+    'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠',
+    'Ы': 'Ы̋͠',
+    'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠',
+    'Ю': 'Ю̋͠',
+    'Я': 'Я̋',
     ' ': ' '
 }
 
@@ -40,36 +84,39 @@ skills = (
     'Огненный заряд'
     )
 
+
 def main():
-    if __name__ == '__main__':
-        file_path = "персонажи"
-        if not os.path.isdir(file_path):
-            os.mkdir(file_path)
-
-        for number in range(10):
-            random_name = Faker("ru_RU").first_name_male()
-            random_surname = Faker("ru_RU").last_name_male()
-            random_job = Faker("ru_RU").job()
-            random_city = Faker("ru_RU").city()
-
-            current_skills = sample(skills, 3)
-            runic_skills = []
-            for skill in current_skills:
-                for letter in skill:
-                    skill = skill.replace(letter, runic_alphabet[letter])
-                runic_skills.append(skill)
-            context = {
-                "first_name":random_name,
-                "last_name":random_surname,
-                "job":random_job,
-                "town":random_city,
-                "strength":randint(3,18),
-                "agility":randint(3,18),
-                "endurance":randint(3,18),
-                "intelligence":randint(3,18),
-                "luck":randint(3,18),
-                "skill_1":runic_skills[0],
-                "skill_2":runic_skills[1],
-                "skill_3":runic_skills[2]
-            }
-            file_operations.render_template("charsheet.svg",f"{file_path}/character{number+1}.svg",context)
+    file_path = "персонажи"
+    os.makedirs(file_path, exist_ok=True)
+    
+    for number in range(10):
+        random_name = Faker("ru_RU").first_name_male()
+        random_surname = Faker("ru_RU").last_name_male()
+        random_job = Faker("ru_RU").job()
+        random_city = Faker("ru_RU").city()
+        
+        current_skills = sample(skills, 3)
+        runic_skills = []
+        for skill in current_skills:
+            for letter in skill:
+                skill = skill.replace(letter, runic_alphabet[letter])
+            runic_skills.append(skill)
+        context = {
+            "first_name": random_name,
+            "last_name": random_surname,
+            "job": random_job,
+            "town": random_city,
+            "strength": randint(3,18),
+            "agility": randint(3,18),
+            "endurance": randint(3,18),
+            "intelligence": randint(3,18),
+            "luck": randint(3,18),
+            "skill_1": runic_skills[0],
+            "skill_2": runic_skills[1],
+            "skill_3": runic_skills[2]
+        }
+        file_operations.render_template("charsheet.svg", f"{file_path}/character{number+1}.svg", context)
+        
+        
+if __name__ == '__main__':
+    main()
